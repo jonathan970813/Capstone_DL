85b49edd768ea42a77ef56c231f9cb2c310bb934
jonathan970813 <jonathan813@naver.com>
2020-04-19 00:53:21 +0900

    for only images Directory

--- renameForTrain.py
+++ renameForTrain.py
@@ -5,11 +5,14 @@ files = listdir('.')
 CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 dir_list = [dir_name for dir_name in files if os.path.isdir(dir_name) and dir_name.startswith('.') != True]
 IMAGE_FORMAT = '.png'
-print(dir_list)
-for dir_name in dir_list:
-    full_dir_name = os.path.join(CUR_PATH, dir_name)
-    file_names = listdir(full_dir_name)
-    image_names = [image_name for image_name in file_names if image_name.endswith(IMAGE_FORMAT)]
-    for image_name in image_names: 
-        new_name = image_name.replace(' ', '')
-        rename(os.path.join(full_dir_name, image_name), os.path.join(full_dir_name, new_name))
+
+full_dir_name = os.path.join(CUR_PATH, 'images')
+if os.path.isdir(full_dir_name) == False:
+    print(full_dir_name, "dosen't exist")
+    exit()
+    
+file_names = listdir(full_dir_name)
+image_names = [image_name for image_name in file_names if image_name.endswith(IMAGE_FORMAT)]
+for image_name in image_names: 
+    new_name = image_name.replace(' ', '')
+    rename(os.path.join(full_dir_name, image_name), os.path.join(full_dir_name, new_name))
